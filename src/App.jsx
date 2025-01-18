1b1b364f8c5334993be14c9cc1cf3201fe40872c
Nagender kumar <kumarnagender811@gmail.com>
2025-01-18 12:38:19 +0530

    add API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
+import { getPost } from './api/PostApi';
 import './App.css'
 
 function App() {
 
+  const getPostData = async () => {
+    const res = await getPost()
+    console.log(res.data);
+    
+  }
+  
+  useEffect(() => {
+    getPostData()
+  }, [])
+
   return (
+    
    <div>
     Hello world
    </div>
